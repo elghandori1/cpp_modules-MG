105e66dffb09d62122b0b935d61797f3b2938ea6
elghandori1 <mohamedelghandori4@gmail.com>
2024-10-09 12:52:18 +0100

    ex01 cpp03 with copy constructor&overloading assignment example in main

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,11 +1,40 @@
 #include "ClapTrap.hpp"
 
-int main()
-{
+int main(){
+   
     ClapTrap claptrap("CLTP");
     claptrap.attack("target1");
     claptrap.takeDamage(5);
     claptrap.beRepaired(4);
+
+    /*
+    //--------test 2:
+        ClapTrap claptrap("CLTP");
+        claptrap.attack("target1");
+        claptrap.takeDamage(5);
+        claptrap.beRepaired(21);
+        claptrap.takeDamage(27);
+        claptrap.beRepaired(21);
+    */
+    /*
+    //------copy constructor:
+
+        ClapTrap claptrap("CLTP");
+        ClapTrap claptrap2(claptrap);
+        claptrap2.attack("enemy");
+        claptrap2.takeDamage(6);
+        claptrap2.takeDamage(2);
+    */
+
+    /*
+    //------ overloading assignment:
+  
+        ClapTrap claptrap("CLTP");
+        ClapTrap claptrap2 = claptrap;
+        claptrap2.attack("enemy");
+        claptrap2.takeDamage(6);
+        claptrap2.takeDamage(2);
+    */
 
     return 0;
 }
